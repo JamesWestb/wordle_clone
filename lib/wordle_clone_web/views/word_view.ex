7db929f0883dd23929f9cb4335ffa786cc9fde7b
James Westbrook <jwestbrook026@gmail.com>
2023-06-16 18:27:31 -0700

    Updated template

--- lib/wordle_clone_web/views/word_view.ex
+++ lib/wordle_clone_web/views/word_view.ex
@@ -3,10 +3,12 @@ defmodule WordleCloneWeb.WordView do
 
   def text_input_grid do
     ~E"""
-    <div class="grid grid-cols-5 col-span-1 gap-2">
-      <%= for _ <- 0..5 do %>
-        <%= text_input_row() %>
-      <% end %>
+    <div class="grid grid-cols-1 self-end mb-4 gap-y-2">
+      <div class="grid grid-cols-5 col-span-1 gap-2">
+        <%= for _ <- 0..5 do %>
+          <%= text_input_row() %>
+        <% end %>
+      </div>
     </div>
     """
   end
@@ -22,7 +24,7 @@ defmodule WordleCloneWeb.WordView do
   defp text_input_cell do
     ~E"""
     <div class="relative w-16 h-16">
-      <input type="text" class="w-full h-full p-4 border-2 border-gray-500 rounded-sm outline-none focus:ring-0 focus:border-gray-500 bg-transparent" maxlength="1">
+      <input type="text" class="w-full h-full p-4 border-2 border-gray-500 rounded-sm outline-none focus:ring-0 focus:border-gray-500 cursor-default bg-transparent" maxlength="1">
     </div>
     """
   end
@@ -35,7 +37,7 @@ defmodule WordleCloneWeb.WordView do
     ]
 
     ~E"""
-    <div class="w-full">
+    <div class="w-full h-1/3">
       <%= for keyboard_row <- keyboard_rows do %>
         <div class="flex justify-center gap-1 my-1 w-full">
           <%= for value <- keyboard_row do %>
@@ -47,9 +49,9 @@ defmodule WordleCloneWeb.WordView do
     """
   end
 
-  defp keyboard_cell("backspace") do
+  defp keyboard_cell(value) when value == "backspace" do
     ~E"""
-    <kbd class="kbd kbd-lg">
+    <kbd id="value" class="kbd kbd-lg">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
         <path stroke-linecap="round" stroke-linejoin="round" d="M12 9.75L14.25 12m0 0l2.25 2.25M14.25 12l2.25-2.25M14.25 12L12 14.25m-2.58 4.92l-6.375-6.375a1.125 1.125 0 010-1.59L9.42 4.83c.211-.211.498-.33.796-.33H19.5a2.25 2.25 0 012.25 2.25v10.5a2.25 2.25 0 01-2.25 2.25h-9.284c-.298 0-.585-.119-.796-.33z" />
       </svg>
@@ -59,7 +61,7 @@ defmodule WordleCloneWeb.WordView do
 
   defp keyboard_cell(value) do
     ~E"""
-    <kbd class="kbd kbd-lg"><%= value %></kbd>
+    <kbd id="value" class="kbd kbd-lg cursor-pointer"><%= value %></kbd>
     """
   end
 end
