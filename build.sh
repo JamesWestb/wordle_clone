ed8f9021851e9b0468bb816997cebac2a130fd50
James Westbrook <jwestbrook026@gmail.com>
2023-06-28 00:58:18 -0700

    build.sh

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Initial setup
+mix deps.get --only prod
+MIX_ENV=prod mix compile
+
+# Compile assets
+npm install --prefix ./assets
+npm run deploy --prefix ./assets
+mix phx.digest
+
+# Build the release and overwrite the existing release directory
+MIX_ENV=prod mix release --overwrite
+
+_build/prod/rel/wordle_clone/bin/wordle_clone eval "WordleClone.Release.migrate"
+_build/prod/rel/wordle_clone/bin/wordle_clone eval "WordleClone.Release.migrate_data"
