9918a3f91cb98e71e1fa4dc4535fcbb1a6cea5a5
James Westbrook <jwestbrook026@gmail.com>
2023-07-02 04:56:01 -0700

    Keyboard element click events

--- lib/wordle_clone_web/views/word_view.ex
+++ lib/wordle_clone_web/views/word_view.ex
@@ -57,7 +57,7 @@ defmodule WordleCloneWeb.WordView do
 
   defp keyboard_cell(value) when value == "backspace" do
     ~E"""
-    <kbd id="value" class="kbd kbd-lg cursor-pointer rounded-md">
+    <kbd id="value" class="kbd kbd-lg cursor-default rounded-md" phx-click="keydown" phx-value-key="Backspace">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7">
         <path stroke-linecap="round" stroke-linejoin="round" d="M12 9.75L14.25 12m0 0l2.25 2.25M14.25 12l2.25-2.25M14.25 12L12 14.25m-2.58 4.92l-6.375-6.375a1.125 1.125 0 010-1.59L9.42 4.83c.211-.211.498-.33.796-.33H19.5a2.25 2.25 0 012.25 2.25v10.5a2.25 2.25 0 01-2.25 2.25h-9.284c-.298 0-.585-.119-.796-.33z" />
       </svg>
@@ -67,13 +67,13 @@ defmodule WordleCloneWeb.WordView do
 
   defp keyboard_cell(value) when value == "enter" do
     ~E"""
-    <kbd id="<%= value %>" class="kbd kbd-lg cursor-pointer font-bold rounded-md py-3 text-xs"><%= String.upcase(value) %></kbd>
+    <kbd id="<%= value %>" class="kbd kbd-lg cursor-default font-bold rounded-md py-3 text-xs"  phx-click="keydown" phx-value-key="Enter"><%= String.upcase(value) %></kbd>
     """
   end
 
   defp keyboard_cell(value) do
     ~E"""
-    <kbd id="<%= value %>" class="kbd kbd-lg cursor-pointer font-bold rounded-md py-3 px-1"><%= String.upcase(value) %></kbd>
+    <kbd id="<%= value %>" class="kbd kbd-lg cursor-default font-bold rounded-md py-3 px-1" phx-click="keydown" phx-value-key=<%= value %>><%= String.upcase(value) %></kbd>
     """
   end
 
