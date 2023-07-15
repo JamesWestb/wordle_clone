e12e3987198e915987b0e713a8b9193197cc22b8
James Westbrook <jwestbrook026@gmail.com>
2023-07-15 09:54:59 -0700

    Key escape modals

--- lib/wordle_clone_web/live/word_live/game_over_component.ex
+++ lib/wordle_clone_web/live/word_live/game_over_component.ex
@@ -1,6 +1,8 @@
 defmodule WordleCloneWeb.WordLive.GameOverComponent do
   use WordleCloneWeb, :live_component
 
+  alias WordleCloneWeb.Router.Helpers, as: Routes
+
   @impl true
   def update(%{title: title, game_win: game_win, answer: answer}, socket) do
     socket
@@ -9,4 +11,13 @@ defmodule WordleCloneWeb.WordLive.GameOverComponent do
     |> assign(:answer, answer)
     |> ok()
   end
+
+  @impl true
+  def handle_event("keydown", %{"key" => "Enter"}, socket) do
+    socket
+    |> push_redirect(to: Routes.word_index_path(socket, :index))
+    |> noreply()
+  end
+
+  def handle_event("keydown", _params, socket), do: noreply(socket)
 end
--- lib/wordle_clone_web/live/word_live/info_component.ex
+++ lib/wordle_clone_web/live/word_live/info_component.ex
@@ -1,10 +1,21 @@
 defmodule WordleCloneWeb.WordLive.InfoComponent do
   use WordleCloneWeb, :live_component
 
+  alias WordleCloneWeb.Router.Helpers, as: Routes
+
   @impl true
   def update(%{title: title}, socket) do
     socket
     |> assign(:title, title)
     |> ok()
+  end
+
+  @impl true
+  def handle_event("keydown", %{"key" => "Meta"}, socket), do: noreply(socket)
+
+  def handle_event("keydown", _params, socket) do
+    socket
+    |> push_redirect(to: Routes.word_index_path(socket, :index))
+    |> noreply()
   end
 end
